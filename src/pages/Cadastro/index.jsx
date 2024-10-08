78223c8cc0c0b87260c99e441e1dbbc740109896
maxmalato-dell-3567 <max.malato@hotmail.com>
2024-10-08 17:51:46 -0300

    Validação dos campos

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom"
 import { useRef, useState } from "react"
+import api from "../../services/api.js"
 
+// COMPONENTES
 import CustomInput from "../../components/CustomInput"
 import CustomButton from "../../components/CustomButton"
 import Header from "../../components/Header"
@@ -15,15 +17,51 @@ export default function Cadastro() {
     const [message, setMessage] = useState('')
     const [messageType, setMessageType] = useState('')
 
-    const handleSubmit = (e) => {
+    const validatePassword = (password) => {
+        const minLength = 6
+        return password.length >= minLength
+    }
+
+    const validateEmail = (email) => {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(String(email).toLocaleLowerCase())
+    }
+
+    const handleSubmit = async (e) => {
         e.preventDefault()
+
+        if (!validateEmail(emailRef.current.value)) {
+            setMessage("Por favor, informe um e-mail válido.")
+            setMessageType("error")
+            return
+        }
 
         if (passwordRef.current.value !== confirmPasswordRef.current.value) {
             setMessage("As senhas não iguais.")
             setMessageType("error")
-        } else {
+            return
+        }
+
+        if (!validatePassword(passwordRef.current.value || confirmPasswordRef.current.value)) {
+            setMessage("Precisa ter mais de seis dígitos.")
+            setMessageType("error")
+            return
+        }
+
+        try {
+            await api.post('/register', {
+                name: nameRef.current.value,
+                email: emailRef.current.value,
+                password: passwordRef.current.value,
+                confirmPassword: confirmPasswordRef.current.value
+            })
             setMessage("Cadastrado com sucesso!")
             setMessageType("success")
+
+        } catch (error) {
+            setMessage("Erro de envio 422. Verifique com o suporte.")
+            setMessageType("Error")
+            console.log(error)
         }
     }
 
@@ -32,9 +70,8 @@ export default function Cadastro() {
             <Header label="Cadastro" />
 
             <form className="flex flex-col gap-4 w-72" onSubmit={handleSubmit}>
-
                 <CustomInput type="text" placeholder="Nome" ref={nameRef} required />
-                <CustomInput type="email" placeholder="Email" ref={emailRef} required />
+                <CustomInput type="text" placeholder="Email" ref={emailRef} required />
                 <CustomInput type="password" placeholder="Senha" ref={passwordRef} required />
                 <CustomInput type="password" placeholder="Repita sua senha" ref={confirmPasswordRef} required />
 
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,17 +1,39 @@
 import { Link } from "react-router-dom"
+import { useRef } from "react"
+import api from "../../services/api"
 
+// COMPONENTES
 import CustomButton from "../../components/CustomButton"
 import CustomInput from "../../components/CustomInput"
 import Header from "../../components/Header"
 
 export default function Login() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const { data: token } = await api.post('/login', {
+                email: emailRef.current.value,
+                password: passwordRef.current.value,
+            })
+
+            localStorage.setItem('token', token.token)
+        } catch (error) {
+            alert("Erro")
+            console.log(error)
+        }
+    }
+
     return (
         <div className="flex flex-col items-center">
             <Header label="Login" />
 
-            <form className="flex flex-col gap-4 w-72">
-                <CustomInput type="email" placeholder="Email" required />
-                <CustomInput type="password" placeholder="Senha" required />
+            <form className="flex flex-col gap-4 w-72" onSubmit={handleSubmit}>
+                <CustomInput type="email" placeholder="Email" ref={emailRef} required />
+                <CustomInput type="password" placeholder="Senha" ref={passwordRef} required />
 
                 <CustomButton label="Acessar" />
             </form>
