4eb9a3d9d247ffc0bb100f87ee365c7929f93a73
maxmalato-dell-3567 <max.malato@hotmail.com>
2024-10-11 11:53:10 -0300

    Página de listagem - estilização

--- src/pages/Lista/index.jsx
+++ src/pages/Lista/index.jsx
@@ -1,20 +1,43 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import api from "../../services/api"
 
 function ListaUsuario() {
+    const [users, setUsers] = useState([])
+
     useEffect(() => {
         async function loadUsers() {
-            const users = await api.get('/listUsers')
+            // PEGAR O TOKEN
+            const token = localStorage.getItem('token')
 
-            console.log(users)
+            // INSERIR O TOKEN
+            const { data: { allUsers } } = await api.get('/listUsers', {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+
+            setUsers(allUsers)
         }
 
         loadUsers()
     }, [])
 
     return (
-        <div>
-            <h2>Listar Usuários</h2>
+        <div className="flex flex-col">
+            <h2 className="text-2xl text-center">Lista de Usuários</h2>
+            <ul className="flex flex-wrap gap-6 justify-center	">
+                {users.length > 0 ? (
+                    users.map((user) => (
+                        <li key={user.id} className="bg-white rounded-lg p-3 flex flex-col">
+                            <p>ID: {user.id}</p>
+                            <p>Nome: {user.name}</p>
+                            <p>E-mail: {user.email}</p>
+                        </li>
+                    ))
+                ) : (
+                    <p>
+                        Nenhum usuário encontrado.
+                    </p>
+                )}
+            </ul>
         </div>
     )
 }
